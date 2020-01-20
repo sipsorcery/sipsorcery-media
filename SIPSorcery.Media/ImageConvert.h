a66f90bee0d0a8189b05f0cd7b247dd66751ef17
Aaron Clauson <aaron@sipsorcery.com>
2020-01-20 13:57:38 +0000

    Renamed and tidied up the DTLS and SRTP classes.

--- SIPSorcery.Media/ImageConvert.h
+++ SIPSorcery.Media/ImageConvert.h
@@ -6,10 +6,10 @@
 
 extern "C"
 {
-#include "libswscale\swscale.h"
-#include "libavcodec\avcodec.h"
-#include "libavformat\avformat.h"
-#include "libavutil\avutil.h"
+#include <libswscale/swscale.h>
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
 }
 
 using namespace System;
