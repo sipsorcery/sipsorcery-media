b869bf9868cffe56f6b42e51f01c08d5a2d19ded
Aaron Clauson <aaron@sipsorcery.com>
2020-02-22 21:17:12 +0000

    SIP video call working.

--- src/SIPSorcery.RtpAVSession/RtpAVSession.cs
+++ src/SIPSorcery.RtpAVSession/RtpAVSession.cs
@@ -15,17 +15,53 @@
 //-----------------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Microsoft.Extensions.Logging;
 using NAudio.Wave;
+using SIPSorcery.Net;
 using SIPSorcery.SIP.App;
+using SIPSorcery.Sys;
+using SIPSorceryMedia;
 
-namespace SIPSorcery.Net
+namespace SIPSorcery.Media
 {
+    public enum AudioSourcesEnum
+    {
+        None = 0,
+        Microphone = 1,
+        Music = 2,
+        Silence = 3
+    }
+
+    public class AudioSourceOptions
+    {
+        public AudioSourcesEnum AudioSource;
+        public string SourceFile;
+    }
+
+    public enum VideoSourcesEnum
+    {
+        None = 0,
+        Webcam = 1,
+        TestPattern = 2
+    }
+
+    public class VideoSourceOptions
+    {
+        public VideoSourcesEnum VideoSource;
+        public string SourceFile;
+    }
+
     public class RtpAVSession : RTPMediaSession
     {
-        public const int AUDIO_SAMPLE_PERIOD_MILLISECONDS = 20;
+        private const int AUDIO_SAMPLE_PERIOD_MILLISECONDS = 30;
+        private const int VP8_TIMESTAMP_SPACING = 3000;
 
         /// <summary>
         /// PCMU encoding for silence, http://what-when-how.com/voip/g-711-compression-voip/
@@ -35,7 +71,11 @@ namespace SIPSorcery.Net
 
         private static Microsoft.Extensions.Logging.ILogger Log = SIPSorcery.Sys.Log.Logger;
 
-        private WaveFormat _waveFormat = new WaveFormat(8000, 16, 1);
+        private static readonly WaveFormat _waveFormat = new WaveFormat(8000, 16, 1);
+
+        private CancellationTokenSource _cts = new CancellationTokenSource();
+        private AudioSourceOptions _audioSourceOpts;
+        private VideoSourceOptions _videoSourceOpts;
 
         /// <summary>
         /// Audio render device.
@@ -52,19 +92,57 @@ namespace SIPSorcery.Net
         /// </summary>
         private WaveInEvent _waveInEvent;
 
+        private static byte[] _currVideoFrame = new byte[65536];
+        private static int _currVideoFramePosn = 0;
+
+        private VpxEncoder _vpxDecoder;
+        private ImageConvert _imgConverter;
+        private PictureBox _videoBox;
+
+        /// <summary>
+        /// Dummy video source which supplies a test pattern with a rolling 
+        /// timestamp.
+        /// </summary>
+        private TestPatternVideoSource _testPatternVideoSource;
+
         private uint _rtpAudioTimestamp = 0;
         private uint _rtpAudioTimestampPeriod = 0;
+        private uint _rtpVideoTimestamp = 0;
         private bool _isClosed = false;
 
-        public RtpAVSession(SDPMediaTypesEnum mediaType, SDPMediaFormat codec, AddressFamily addrFamily)
-            : base(mediaType, codec, addrFamily)
+        public RtpAVSession(AddressFamily addrFamily, AudioSourceOptions audioSourceOptions, VideoSourceOptions videoSourceOptions, PictureBox videoBox)
+            : base(addrFamily)
         {
-            base.OnRtpPacketReceived += RtpPacketReceived;
+            _audioSourceOpts = audioSourceOptions;
+            _videoSourceOpts = videoSourceOptions;
+            _videoBox = videoBox;
 
-            if (mediaType == SDPMediaTypesEnum.audio)
+            _vpxDecoder = new VpxEncoder();
+            int res = _vpxDecoder.InitDecoder();
+            if (res != 0)
             {
-                InitAudioDevices();
+                throw new ApplicationException("VPX decoder initialisation failed.");
             }
+
+            _imgConverter = new ImageConvert();
+
+            if (_audioSourceOpts != null && _audioSourceOpts.AudioSource != AudioSourcesEnum.None)
+            {
+                MediaStreamTrack videoTrack = new MediaStreamTrack(null, SDPMediaTypesEnum.audio, false, new List<SDPMediaFormat> { new SDPMediaFormat(SDPMediaFormatsEnum.PCMU) });
+                addTrack(videoTrack);
+
+                InitAudio(_audioSourceOpts.AudioSource);
+            }
+
+            if (_videoSourceOpts != null && _videoSourceOpts.VideoSource != VideoSourcesEnum.None)
+            {
+                MediaStreamTrack videoTrack = new MediaStreamTrack(null, SDPMediaTypesEnum.video, false, new List<SDPMediaFormat> { new SDPMediaFormat(SDPMediaFormatsEnum.VP8) });
+                addTrack(videoTrack);
+
+                InitVideo(_videoSourceOpts.VideoSource);
+            }
+
+            base.OnRtpPacketReceived += RtpPacketReceived;
         }
 
         /// <summary>
@@ -73,7 +151,28 @@ namespace SIPSorcery.Net
         public void Start()
         {
             _waveOutEvent?.Play();
-            _waveInEvent?.StartRecording();
+
+            AudioSourcesEnum audioSource = (_audioSourceOpts != null) ? _audioSourceOpts.AudioSource : AudioSourcesEnum.None;
+
+            if (audioSource == AudioSourcesEnum.Microphone && _waveInEvent != null)
+            {
+                _waveInEvent?.StartRecording();
+            }
+            else if (audioSource == AudioSourcesEnum.Silence)
+            {
+                Task.Run(() => SendSilence(_cts.Token));
+            }
+            else if (audioSource == AudioSourcesEnum.Music)
+            {
+                Task.Run(() => SendMusic(_cts.Token));
+            }
+
+            VideoSourcesEnum videoSource = (_videoSourceOpts != null) ? _videoSourceOpts.VideoSource : VideoSourcesEnum.None;
+
+            if (videoSource == VideoSourcesEnum.TestPattern && _testPatternVideoSource != null)
+            {
+                Task.Run(() => _testPatternVideoSource.Start(_cts.Token));
+            }
         }
 
         public override void Close()
@@ -86,6 +185,7 @@ namespace SIPSorcery.Net
 
                 _waveOutEvent?.Stop();
                 _waveInEvent?.StopRecording();
+                _cts.Cancel();
 
                 base.Close();
             }
@@ -94,7 +194,7 @@ namespace SIPSorcery.Net
         /// <summary>
         /// Initialise the audio capture and render device.
         /// </summary>
-        private void InitAudioDevices()
+        private void InitAudio(AudioSourcesEnum audioSource)
         {
             // Render device.
             _waveOutEvent = new WaveOutEvent();
@@ -102,25 +202,37 @@ namespace SIPSorcery.Net
             _waveProvider.DiscardOnBufferOverflow = true;
             _waveOutEvent.Init(_waveProvider);
 
-            // Capture device.
-            if (WaveInEvent.DeviceCount > 0)
+            // Audio source.
+            if (audioSource == AudioSourcesEnum.Microphone)
             {
-                _waveInEvent = new WaveInEvent();
-                _waveInEvent.BufferMilliseconds = AUDIO_SAMPLE_PERIOD_MILLISECONDS;
-                _waveInEvent.NumberOfBuffers = 1;
-                _waveInEvent.DeviceNumber = 0;
-                _waveInEvent.WaveFormat = _waveFormat;
-                _waveInEvent.DataAvailable += LocalAudioSampleAvailable;
-            }
-            else
-            {
-                Log.LogWarning("No audio capture devices are available. A dummy silence stream will be sent.");
-
-                // Send dummy silence audio packets to the remote party.
-                _ = Task.Run(SendSilence);
+                if (WaveInEvent.DeviceCount > 0)
+                {
+                    _waveInEvent = new WaveInEvent();
+                    _waveInEvent.BufferMilliseconds = AUDIO_SAMPLE_PERIOD_MILLISECONDS;
+                    _waveInEvent.NumberOfBuffers = 1;
+                    _waveInEvent.DeviceNumber = 0;
+                    _waveInEvent.WaveFormat = _waveFormat;
+                    _waveInEvent.DataAvailable += LocalAudioSampleAvailable;
+                }
+                else
+                {
+                    Log.LogWarning("No audio capture devices are available. No audio stream will be sent.");
+                }
             }
 
             _rtpAudioTimestampPeriod = (uint)(SDPMediaFormatInfo.GetClockRate(SDPMediaFormatsEnum.PCMU) / AUDIO_SAMPLE_PERIOD_MILLISECONDS);
+        }
+
+        /// <summary>
+        /// Initialise the video capture and render device.
+        /// </summary>
+        private void InitVideo(VideoSourcesEnum videoSource)
+        {
+            if (videoSource == VideoSourcesEnum.TestPattern)
+            {
+                _testPatternVideoSource = new TestPatternVideoSource();
+                _testPatternVideoSource.SampleReady += LocalVideoSampleAvailable;
+            }
         }
 
         /// <summary>
@@ -142,12 +254,23 @@ namespace SIPSorcery.Net
         }
 
         /// <summary>
+        /// Event handler for video sample being supplied by local capture device.
+        /// </summary>
+        private void LocalVideoSampleAvailable(byte[] sample)
+        {
+            base.SendVp8Frame(_rtpVideoTimestamp, (int)SDPMediaFormatsEnum.VP8, sample);
+            _rtpVideoTimestamp += VP8_TIMESTAMP_SPACING;
+        }
+
+        /// <summary>
         /// Event handler for receiving RTP packets from a remote party.
         /// </summary>
         /// <param name="mediaType">The media type of the packets.</param>
         /// <param name="rtpPacket">The RTP packet with the media sample.</param>
         private void RtpPacketReceived(SDPMediaTypesEnum mediaType, RTPPacket rtpPacket)
         {
+            //Log.LogDebug($"RTP packet received for {mediaType}.");
+
             if (mediaType == SDPMediaTypesEnum.audio)
             {
                 RenderAudio(rtpPacket);
@@ -179,19 +302,105 @@ namespace SIPSorcery.Net
         /// <param name="rtpPacket">The RTP packet containing the video payload.</param>
         private void RenderVideo(RTPPacket rtpPacket)
         {
-            throw new NotImplementedException();
+            if (_currVideoFramePosn > 0 || (rtpPacket.Payload[0] & 0x10) > 0)
+            {
+                // TODO: use the VP8 Payload descriptor to properly determine the VP8 header length (currently hard coded to 4).
+                Buffer.BlockCopy(rtpPacket.Payload, 4, _currVideoFrame, _currVideoFramePosn, rtpPacket.Payload.Length - 4);
+                _currVideoFramePosn += rtpPacket.Payload.Length - 4;
+
+                if (rtpPacket.Header.MarkerBit == 1)
+                {
+                    unsafe
+                    {
+                        fixed (byte* p = _currVideoFrame)
+                        {
+                            uint width = 0, height = 0;
+                            byte[] i420 = null;
+
+                            //Console.WriteLine($"Attempting vpx decode {_currVideoFramePosn} bytes.");
+
+                            int decodeResult = _vpxDecoder.Decode(p, _currVideoFramePosn, ref i420, ref width, ref height);
+
+                            if (decodeResult != 0)
+                            {
+                                Console.WriteLine("VPX decode of video sample failed.");
+                            }
+                            else
+                            {
+                                //Console.WriteLine($"Video frame ready {width}x{height}.");
+
+                                fixed (byte* r = i420)
+                                {
+                                    byte[] bmp = null;
+                                    int stride = 0;
+                                    int convRes = _imgConverter.ConvertYUVToRGB(r, VideoSubTypesEnum.I420, (int)width, (int)height, VideoSubTypesEnum.BGR24, ref bmp, ref stride);
+
+                                    if (convRes == 0)
+                                    {
+                                        _videoBox.BeginInvoke(new Action(() =>
+                                        {
+                                            fixed (byte* s = bmp)
+                                            {
+                                                System.Drawing.Bitmap bmpImage = new System.Drawing.Bitmap((int)width, (int)height, stride, System.Drawing.Imaging.PixelFormat.Format24bppRgb, (IntPtr)s);
+                                                _videoBox.Image = bmpImage;
+                                            }
+                                        }));
+                                    }
+                                    else
+                                    {
+                                        Log.LogWarning("Pixel format conversion of decoded sample failed.");
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    _currVideoFramePosn = 0;
+                }
+            }
+            else
+            {
+                Log.LogWarning("Discarding RTP packet, VP8 header Start bit not set.");
+                Log.LogWarning($"rtp video, seqnum {rtpPacket.Header.SequenceNumber}, ts {rtpPacket.Header.Timestamp}, marker {rtpPacket.Header.MarkerBit}, payload {rtpPacket.Payload.Length}, payload[0-5] {rtpPacket.Payload.HexStr(5)}.");
+            }
         }
 
         /// <summary>
         /// Sends the sounds of silence. If the destination is on the other side of a NAT this is useful to open
         /// a pinhole and hopefully get the remote RTP stream through.
         /// </summary>
-        private async void SendSilence()
+        private async void SendMusic(CancellationToken ct)
+        {
+            uint rtpSampleTimestamp = 0;
+
+            using (StreamReader sr = new StreamReader(_audioSourceOpts.SourceFile))
+            {
+                int sampleSize = (SDPMediaFormatInfo.GetClockRate(SDPMediaFormatsEnum.PCMU) / 1000) * AUDIO_SAMPLE_PERIOD_MILLISECONDS;
+                byte[] sample = new byte[sampleSize];
+                int bytesRead = sr.BaseStream.Read(sample, 0, sample.Length);
+
+                while (bytesRead > 0 && !ct.IsCancellationRequested)
+                {
+                    SendAudioFrame(rtpSampleTimestamp, (int)SDPMediaFormatsEnum.PCMU, sample);
+                    rtpSampleTimestamp += _rtpAudioTimestampPeriod;
+
+                    bytesRead = sr.BaseStream.Read(sample, 0, sample.Length);
+
+                    await Task.Delay(AUDIO_SAMPLE_PERIOD_MILLISECONDS);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sends the sounds of silence. If the destination is on the other side of a NAT this is useful to open
+        /// a pinhole and hopefully get the remote RTP stream through.
+        /// </summary>
+        private async void SendSilence(CancellationToken ct)
         {
             uint bufferSize = (uint)AUDIO_SAMPLE_PERIOD_MILLISECONDS;
             uint rtpSampleTimestamp = 0;
 
-            while (!_isClosed)
+            while (!ct.IsCancellationRequested)
             {
                 byte[] sample = new byte[bufferSize / 2];
                 int sampleIndex = 0;
---
+++ src/SIPSorcery.RtpAVSession/TestPatternVideoSource.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using SIPSorceryMedia;
+
+namespace SIPSorcery.Media
+{
+    public class TestPatternVideoSource
+    {
+        private static string TEST_PATTERN_IMAGE_PATH = "media/testpattern.jpeg";
+        private const float TEXT_SIZE_PERCENTAGE = 0.035f;       // height of text as a percentage of the total image height
+        private const float TEXT_OUTLINE_REL_THICKNESS = 0.02f; // Black text outline thickness is set as a percentage of text height in pixels
+        private const int TEXT_MARGIN_PIXELS = 5;
+        private const int POINTS_PER_INCH = 72;
+
+        private static Microsoft.Extensions.Logging.ILogger logger = SIPSorcery.Sys.Log.Logger;
+
+        public event Action<byte[]> SampleReady;
+
+        public TestPatternVideoSource()
+        { }
+
+        public void Start(CancellationToken ct)
+        {
+            try
+            {
+                unsafe
+                {
+                    Bitmap testPattern = new Bitmap(TEST_PATTERN_IMAGE_PATH);
+
+                    // Get the stride.
+                    Rectangle rect = new Rectangle(0, 0, testPattern.Width, testPattern.Height);
+                    System.Drawing.Imaging.BitmapData bmpData =
+                        testPattern.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                        testPattern.PixelFormat);
+
+                    // Get the address of the first line.
+                    int stride = bmpData.Stride;
+
+                    testPattern.UnlockBits(bmpData);
+
+                    // Initialise the video codec and color converter.
+                    SIPSorceryMedia.VpxEncoder vpxEncoder = new VpxEncoder();
+                    vpxEncoder.InitEncoder((uint)testPattern.Width, (uint)testPattern.Height, (uint)stride);
+
+                    SIPSorceryMedia.ImageConvert colorConverter = new ImageConvert();
+
+                    byte[] sampleBuffer = null;
+                    byte[] encodedBuffer = null;
+
+                    while (!ct.IsCancellationRequested)
+                    {
+                        if (SampleReady != null)
+                        {
+                            var stampedTestPattern = testPattern.Clone() as System.Drawing.Image;
+                            AddTimeStampAndLocation(stampedTestPattern, DateTime.UtcNow.ToString("dd MMM yyyy HH:mm:ss:fff"), "Test Pattern");
+                            sampleBuffer = BitmapToRGB24(stampedTestPattern as System.Drawing.Bitmap);
+
+                            fixed (byte* p = sampleBuffer)
+                            {
+                                byte[] convertedFrame = null;
+                                colorConverter.ConvertRGBtoYUV(p, VideoSubTypesEnum.BGR24, testPattern.Width, testPattern.Height, stride, VideoSubTypesEnum.I420, ref convertedFrame);
+
+                                fixed (byte* q = convertedFrame)
+                                {
+                                    int encodeResult = vpxEncoder.Encode(q, convertedFrame.Length, 1, ref encodedBuffer);
+
+                                    if (encodeResult != 0)
+                                    {
+                                        logger.LogWarning("VPX encode of video sample failed.");
+                                        continue;
+                                    }
+                                }
+                            }
+
+                            stampedTestPattern.Dispose();
+                            stampedTestPattern = null;
+
+                           SampleReady?.Invoke(encodedBuffer);
+
+                            encodedBuffer = null;
+                        }
+
+                        Thread.Sleep(30);
+                    }
+                }
+            }
+            catch (Exception excp)
+            {
+                logger.LogError("Exception SendTestPattern. " + excp);
+            }
+        }
+
+        private static byte[] BitmapToRGB24(Bitmap bitmap)
+        {
+            try
+            {
+                BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                var length = bitmapData.Stride * bitmapData.Height;
+
+                byte[] bytes = new byte[length];
+
+                // Copy bitmap to byte[]
+                Marshal.Copy(bitmapData.Scan0, bytes, 0, length);
+                bitmap.UnlockBits(bitmapData);
+
+                return bytes;
+            }
+            catch (Exception)
+            {
+                return new byte[] { };
+            }
+        }
+
+        private static void AddTimeStampAndLocation(System.Drawing.Image image, string timeStamp, string locationText)
+        {
+            int pixelHeight = (int)(image.Height * TEXT_SIZE_PERCENTAGE);
+
+            Graphics g = Graphics.FromImage(image);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+            using (StringFormat format = new StringFormat())
+            {
+                format.LineAlignment = StringAlignment.Center;
+                format.Alignment = StringAlignment.Center;
+
+                using (Font f = new Font("Tahoma", pixelHeight, GraphicsUnit.Pixel))
+                {
+                    using (var gPath = new GraphicsPath())
+                    {
+                        float emSize = g.DpiY * f.Size / POINTS_PER_INCH;
+                        if (locationText != null)
+                        {
+                            gPath.AddString(locationText, f.FontFamily, (int)FontStyle.Bold, emSize, new Rectangle(0, TEXT_MARGIN_PIXELS, image.Width, pixelHeight), format);
+                        }
+
+                        gPath.AddString(timeStamp /* + " -- " + fps.ToString("0.00") + " fps" */, f.FontFamily, (int)FontStyle.Bold, emSize, new Rectangle(0, image.Height - (pixelHeight + TEXT_MARGIN_PIXELS), image.Width, pixelHeight), format);
+                        g.FillPath(Brushes.White, gPath);
+                        g.DrawPath(new Pen(Brushes.Black, pixelHeight * TEXT_OUTLINE_REL_THICKNESS), gPath);
+                    }
+                }
+            }
+        }
+    }
+}
